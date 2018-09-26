21bdf57bc7344618f122907673a4c0efb66a8bee
Joyce Liao <joyliao07@gmail.com>
2018-09-26 12:26:32 -0700

    search page

--- server.js
+++ server.js
@@ -28,58 +28,61 @@ app.get('/', getBooks);
 //   response.render('pages/index');
 // })
 
-
-app.get('/hello', (request, response) => {
-  response.render('pages/index');
+app.get('/searches/new', (request, response) => {
+  response.render('pages/searches/new');
 })
-
 
 app.post('/searches', createSearch);
 
+
 function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-
-  console.log(request.body);
-
   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
 
   superagent.get(url)
-
-    .then(result => {
+    .then ( result => {
       const bookArray = result.body.items.map(book => new Book(book));
       const bookArrayLimit10 = [];
-
       if(bookArray.length > 10) {
         for(let i = 0; i < 10; i++) {
           bookArrayLimit10.push(bookArray[i]);
         }
-        return bookArrayLimit10
+        return bookArrayLimit10;
       }
       return bookArray;
     })
     .then(
       books => {
-        //console.log('bookArray is: ', bookArray);
         response.render('pages/searches/show', {bookList: books});
       })
-    .catch (
-      // error => console.log('An error occurs from createSearch: ', error)
-      error => {
-        // console.log('My-error: ', error);
-        response.render('pages/error', {errorMsg: error});
-      });
+    .catch ( error => {
+      response.render('pages/error', {errorMsg: error});
+    })
 }
+
+
+Book.prototype = {
+  save: function(bookId) {
+    const SQL = `INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUES ($1, $2, $3, $4, $5, $6);`;
+    const values = [this.author, this.title, this.isbn, this.image_url, this.description, this.bookshelf];
+    client.query(SQL, values);
+  }
+};
+
 
 
 function Book (book) {
 
-  const placeHolderImg = 'https://i.imgur.com/J5LVHEL.jpg'
+  const placeHolderImg = 'https://i.imgur.com/J5LVHEL.jpg';
+
   this.title = book.volumeInfo.title || 'No book title found.';
-  this.author = book.volumeInfo.authors[0] ? book.volumeInfo.authors[0] : 'No author found.'; //maybe multiple authors
+  // console.log('book.volumeInfo.authors', book.volumeInfo.authors);
+  this.author = book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'No author found.'; //maybe multiple authors
   this.description = book.volumeInfo.description || 'No description found.';
   this.img_url = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : placeHolderImg;
   this.isbn = book.volumeInfo.industryIdentifiers ? `ISBN: ${book.volumeInfo.industryIdentifiers[0].identifier}` : 'No ISBN found.'
+  this.bookshelf = 'Please put in bookshelf';
 }
 
 // Helper functions
